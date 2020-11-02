02b54db91b1a39e4053801d2359248855e594d43
Unsourced Pyramid <70346064+HellFire0x@users.noreply.github.com>
2020-11-02 20:12:17 -0330

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,34 +6,29 @@ namespace Roblox_UF
 {
     internal static class Program
     {
-        private const string Version = "1.0.1";
-
+        private const string Version = "1.0.1a";
         private static readonly Random Random = new Random();
 
-        private static string RandomString(int l)
-        {
-            const string chars = "zxcvbnmasdfghjklqpwoeirutyABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Repeat(chars, l)
-                .Select(s => s[Random.Next(s.Length)]).ToArray());
-        }
+        private static string RandomString(int l) =>
+            new string(Enumerable.Repeat("zxcvbnmasdfghjklqpwoeirutyABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", l)
+                .Select(s => s[Random.Next(maxValue: s.Length)]).ToArray());
 
         public static void Main()
         {
             var good = 0;
             var bad = 0;
 
-
             Console.Title =
                 $"Good: [{long.Parse(good.ToString()):C0}] | Bad: [{long.Parse(bad.ToString()):C0}] | Version: {Version}";
             Console.Write("Username Length: ");
-            var num = Console.ReadLine();
+            var num = Console.ReadLine() ?? throw new ArgumentNullException($"Console.ReadLine()");
             Console.Clear();
 
             while (true)
             {
                 Console.Title =
                     $"Good: [{long.Parse(good.ToString()):C0}] | Bad: [{long.Parse(bad.ToString()):C0}] | Version: {Version}";
-                var username = RandomString(int.Parse(num ?? throw new InvalidOperationException()));
+                var username = RandomString(int.Parse(num));
                 using (var webClient = new WebClient())
                 {
                     if (webClient.DownloadString($"https://api.roblox.com/users/get-by-username?username={username}")
@@ -43,10 +38,7 @@ namespace Roblox_UF
                         Console.WriteLine($"Good: {username}");
                     }
                     else
-                    {
                         bad++;
-                    }
-
                     webClient.Dispose();
                 }
             }
