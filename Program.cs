6bd3e96955e6ac395f2886514f5c0927b3b70364
Unsourced Pyramid <70346064+HellFire0x@users.noreply.github.com>
2020-11-02 15:47:31 -0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace Roblox_UF
+{
+    internal static class Program
+    {
+        private const string Version = "1.0.0";
+
+        private static readonly Random Random = new Random();
+
+        private static string  RandomString(int l)
+        {
+            const string chars = "zxcvbnmasdfghjklqpwoeirutyABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, l)
+                .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+        
+        public static void Main()
+        {
+            var good = 0;
+            var bad = 0;
+            
+            Console.Title = $"Good: [{good}] | Bad: [{bad}] | Version: {Version}";
+
+            Console.Write("Username Length: ");
+            var num = Console.ReadLine();
+            Console.Clear();
+            
+            while (true)
+            {
+                Console.Title = $"Good: [{good}] | Bad: [{bad}] | Version: {Version}";
+                var username = RandomString(l: int.Parse(num ?? throw new InvalidOperationException()));
+                using (var webClient = new WebClient())
+                {
+                    if (webClient.DownloadString($"https://api.roblox.com/users/get-by-username?username={username}")
+                        .Contains("{\"success\":false,\"errorMessage\":\"User not found\"}"))
+                    {
+                        good++;
+                        Console.WriteLine($"Good: {username}");
+                    }
+                    else
+                    {
+                        bad++;
+                    }
+                    webClient.Dispose();
+                }
+            }
+        }
+    }
+}
